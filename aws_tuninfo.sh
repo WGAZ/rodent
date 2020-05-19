d0e605636d4417782575a0b1793f1fddf1a0eac1
WGAZ <65501062+WGAZ@users.noreply.github.com>
2020-05-19 13:19:20 +0100

    Create aws_tuninfo.sh

---
+++ aws_tuninfo.sh
@@ -0,0 +1,12 @@
+cd /etc/wireguard/clients2/CLIENT
+qrencode -t ansiutf8 < ./CLIENT.conf
+
+# Show config file
+echo "####### This is for your Client (WG1 Tunnel) either scan the above QR with the phone app or Copy the text below to your clients config file wg1.conf ##########"
+cat ./CLIENT.conf
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
